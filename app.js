0ecb2b8a969892ce6275b63bcb2a3d415dcf2b9d
Alan Phelps <phelpsa64@gmail.com>
2022-01-25 18:15:44 -0500

    add inquirer module

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
+// const pageHTML = generatePage(uName, github);
 
-const generatePage = require('./src/page-template.js');
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-const profileDataArgs = process.argv.slice(2);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-const [uName, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(uName, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then (answers => console.log(answers));
