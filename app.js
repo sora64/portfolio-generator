f06e609dc9c07d2280777faa6a3a7e6b1fbd3bf4
Alan Phelps <phelpsa64@gmail.com>
2022-01-27 18:05:16 -0500

    add header, about me, projects, and footer to the created index.html file

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(uName, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -143,5 +135,11 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw new Error(err);
+
+         console.log('Page created! Check out index.html in this directory to see it!');
+        })
     });
